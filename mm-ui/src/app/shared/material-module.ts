e08714de9dc5dee22d2186f94e5ba97817b344dc
Jakub Kuhajda <jakub.geralt.kuhajda@gmail.com>
2025-06-16 18:20:16 +0200

    feat: create MaterialModule for Angular Material components

---
+++ mm-ui/src/app/shared/material-module.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatCardModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatCardModule,
+    MatToolbarModule,
+  ],
+})
+export class MaterialModule {}
