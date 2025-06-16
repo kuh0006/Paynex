a5d80e9bd270f83cc31f2642de3c831dc8006fae
Jakub Kuhajda <jakub.geralt.kuhajda@gmail.com>
2025-06-16 11:27:15 +0200

    commit

--- MM.Contracts/IMerchant/IMerchantRepository.cs
+++ MM.Contracts/IMerchant/IMerchantRepository.cs
@@ -1,7 +1,16 @@
-﻿namespace MM.Contracts.IMerchant
+﻿using MM.Entities.Models;
+
+namespace MM.Contracts.IMerchant
 {
     public interface IMerchantRepository : IRepositoryBase<Merchant>
     {
+        Task<IEnumerable<Merchant>> GetAllAsync();
+        Task<Merchant> GetByIdAsync(int id);
+        Task<Merchant> GetByWithDetailsAsync(int id);
+        Task<Merchant> GetMerchantByNameAsync(string name);
 
+        void CreateProject(Merchant merchant);
+        void UpdateProject(Merchant merchant);
+        void DeleteProject(Merchant merchant);
     }
 }
