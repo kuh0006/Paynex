381a9a987b62ff8188f151f5776f07d43dc34ff3
Jakub Kuhajda <jakub.geralt.kuhajda@gmail.com>
2025-06-17 11:56:19 +0200

    commit

--- mm-ui/src/app/merchants/merchant-dialog/merchant-dialog.component.ts
+++ mm-ui/src/app/merchants/merchant-dialog/merchant-dialog.component.ts
@@ -1,6 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MaterialModule } from '../../shared/material-module';
 import { Merchant, MERCHANT_CATEGORIES } from '../../models/merchant.model';
@@ -10,7 +15,7 @@ import { Merchant, MERCHANT_CATEGORIES } from '../../models/merchant.model';
   templateUrl: './merchant-dialog.component.html',
   styleUrls: ['./merchant-dialog.component.scss'],
   standalone: true,
-  imports: [CommonModule, MaterialModule, ReactiveFormsModule]
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
 })
 export class MerchantDialogComponent implements OnInit {
   merchantForm!: FormGroup;
@@ -22,11 +27,11 @@ export class MerchantDialogComponent implements OnInit {
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<MerchantDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { merchant?: Merchant }
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.isEditMode = !!this.data?.merchant;
-    
+
     if (this.isEditMode) {
       this.dialogTitle = 'Edit Merchant';
     }
@@ -37,9 +42,19 @@ export class MerchantDialogComponent implements OnInit {
   initializeForm(): void {
     this.merchantForm = this.fb.group({
       id: [this.data?.merchant?.id || null],
-      name: [this.data?.merchant?.name || '', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
-      email: [this.data?.merchant?.email || '', [Validators.required, Validators.email]],
-      category: [this.data?.merchant?.category || '', Validators.required]
+      name: [
+        this.data?.merchant?.name || '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100),
+        ],
+      ],
+      email: [
+        this.data?.merchant?.email || '',
+        [Validators.required, Validators.email],
+      ],
+      category: [this.data?.merchant?.category || '', Validators.required],
     });
   }
 
@@ -58,7 +73,7 @@ export class MerchantDialogComponent implements OnInit {
 
   // Helper to mark all controls in a form group as touched
   markFormGroupTouched(formGroup: FormGroup): void {
-    Object.values(formGroup.controls).forEach(control => {
+    Object.values(formGroup.controls).forEach((control) => {
       control.markAsTouched();
 
       if ((control as FormGroup).controls) {
